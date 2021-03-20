5d0bb2bd76074b71d3bab2457d9008a3dfd7c2f9
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-20 19:51:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -119,5 +119,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
